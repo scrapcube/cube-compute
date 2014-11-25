f733146ac3f3a6fa891fa125a903a975d85bcaa8
scrapcube <dmitry.m.kolobov@gmail.com>
2014-11-25 00:25:20 -0700

    Refactor timeline redux.

--- src/learnable/components/inspector.cljs
+++ src/learnable/components/inspector.cljs
@@ -3,37 +3,68 @@
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
 
-(defn restore-to! [process this-time]
-  (om/transact! process #(ps/restore % this-time)))
+(defn track-position [entry-type circle-radius]
+  (* 2 circle-radius (condp = entry-type
+                       :clock 1
+                       :mouse 3
+                       :start 3
+                       :keyboard 5)))
 
-(def conversion-ratio 0.10)
+(defn timeline-entry [process at entry pixel-ratio circle-radius]
+  (dom/li #js {:style #js {:left (* pixel-ratio (last entry))
+                           :width (* 2 circle-radius)}}
+    (dom/a #js {:className "timeline-entry"
+                :key at
+                :onClick
+                  (fn [_]
+                    (om/transact! process #(ps/restore % at)))
+                :style
+                  #js {:top (track-position (first entry) circle-radius)
+                       :width (* 2 circle-radius)
+                       :height (* 2 circle-radius)
+                       :borderRadius circle-radius}}
+      "")))
 
-(defn entry-class [process at entry]
-  (let [classes "timeline-entry "]
-    (str classes
-         (if (= at (get-in process [:log :now]))
-           "current"
-           ""))))
+(defn timeline [process owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:circle-radius 5
+       :min-circle-separation 0
+       :pixel-conversion-ratio 0.10})
 
-(defn timeline-entry [process at entry]
-  (dom/li #js {:style #js {:left (* conversion-ratio (last entry))}}
-    (dom/div #js {:className (entry-class process at entry)}
-      (dom/a #js {:className (name (first entry))
-                  :key at
-                  :onClick (fn [_] (restore-to! process at))}
-        ""))))
+    om/IWillRender
+    (will-render [_]
+      (om/update-state!
+        owner
+        (fn [state]
+          (let [entries (get-in process [:log :entries])
+                {:keys [circle-radius min-circle-separation]} state
+                differentials
+                  (map-indexed #([%2 (nth entries %1)]) entries)
+                min-time-differential
+                  (reduce min (first differentials) (rest differntials))]
+            (assoc state
+              :pixel-conversion-ratio
+                (/ (+ (* 2 circle-radius) min-circle-separation)
+                   (min-time-differential)))))))
+
+    om/IRenderState
+    (render-state [_ {:keys [pixel-conversion-ratio circle-radius]}]
+      (apply dom/ul #js {:className "timeline"
+                         :height (* 2 circle-radius 7)}
+        (cons
+            (timeline-entry process 0 ["start" "" 20] pixel-conversion-ratio circle-radius)
+            (map-indexed
+              #(timeline-entry process (inc %1) %2 pixel-conversion-ratio circle-radius)
+              (get-in process [:log :entries])))))))
 
 (defn ui [process owner]
   (reify
     om/IRender
     (render [_]
       (dom/div #js {:className "inspector"}
-        (apply dom/ul #js {:className "timeline"}
-          (cons
-            (timeline-entry process 0 ["start" "" 20])
-            (map-indexed
-              #(timeline-entry process (inc %1) %2)
-              (get-in process [:log :entries]))))
+        (om/build timeline process)
 
         (apply
           dom/div
